f82e06206ca1b0913c1dcd89bbcc44d8451e0791
Souvik <souvikelric@gmail.com>
2025-03-19 12:22:32 +0530

    added and tested background functions

--- src/index.ts
+++ src/index.ts
@@ -31,12 +31,24 @@ const clientLogger = {
 };
 
 const ansiColors = {
+  fgWhite: "\x1b[37m", //White
   reset: "\x1b[0m",
   info: "\x1b[34m", // Blue
   success: "\x1b[32m", // Green
   warn: "\x1b[33m", // Yellow
   error: "\x1b[31m", // Red
   debug: "\x1b[35m", // Magenta
+};
+
+const bgColors = {
+  reset: "\x1b[0m",
+  error: "\x1b[41m", // Red background
+  success: "\x1b[42m", // Green background
+  warn: "\x1b[43m", // Yellow background
+  info: "\x1b[44m", // Blue background
+  debug: "\x1b[45m", // Magenta background
+  cyan: "\x1b[46m", // Cyan background
+  bgWhite: "\x1b[47m", // White background
 };
 
 class ServerLogger {
@@ -52,24 +64,60 @@ class ServerLogger {
     console.log(formattedMessage);
   }
 
+  static logWithBg(
+    level: keyof typeof bgColors,
+    message: string,
+    date: boolean = false
+  ) {
+    const timestamp = formatDate(new Date());
+    const formattedMessage = `${bgColors[level]}${
+      ansiColors.fgWhite
+    }${message} ${date ? timestamp : ""}${ansiColors.reset} `;
+    console.log(formattedMessage);
+  }
+
   static info(message: string, timestamp?: boolean) {
     this.log("info", message, timestamp);
+  }
+
+  static infoBg(message: string, timestamp?: boolean) {
+    this.logWithBg("info", message, timestamp);
   }
 
   static success(message: string, timestamp?: boolean) {
     this.log("success", message, timestamp);
   }
 
+  static successBg(message: string, timestamp?: boolean) {
+    this.logWithBg("success", message, timestamp);
+  }
+
   static warn(message: string, timestamp?: boolean) {
     this.log("warn", message, timestamp);
+  }
+
+  static warnBg(message: string, timestamp?: boolean) {
+    this.logWithBg("warn", message, timestamp);
   }
 
   static error(message: string, timestamp?: boolean) {
     this.log("error", message, timestamp);
   }
 
+  static errorBg(message: string, timestamp?: boolean) {
+    this.logWithBg("error", message, timestamp);
+  }
+
   static debug(message: string, timestamp?: boolean) {
     this.log("debug", message, timestamp);
+  }
+
+  static debugBg(message: string, timestamp?: boolean) {
+    this.logWithBg("debug", message, timestamp);
+  }
+
+  static cyanBg(message: string, timestamp?: boolean) {
+    this.logWithBg("cyan", message, timestamp);
   }
 }
 
@@ -77,6 +125,6 @@ export { clientLogger, ServerLogger };
 
 let cpuInfo = os.cpus();
 ServerLogger.success(`Machine Model : ${cpuInfo[0].model}`);
-ServerLogger.info(`Number of Cores : ${cpuInfo.length}`);
-ServerLogger.error(`Total Memory : ${os.totalmem()}`);
-ServerLogger.debug(`Free Memory : ${os.freemem()}`, true);
+ServerLogger.infoBg(`Number of Cores : ${cpuInfo.length}`);
+ServerLogger.errorBg(`Total Memory : ${os.totalmem()}`);
+ServerLogger.cyanBg(`Free Memory : ${os.freemem()}`, true);
