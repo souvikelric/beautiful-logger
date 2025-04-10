62dc0bfc7a7e74b19de4c5c0a9825fe4c41f253e
Souvik <souvikelric@gmail.com>
2025-04-10 12:19:19 +0530

    added function to cli tool to comment all console logs

--- src/logs.ts
+++ src/logs.ts
@@ -23,17 +23,55 @@ function getFiles(currdir: string, fileList: string[] = []): string[] {
 }
 
 let logs = 0;
+let commentedLogs = 0;
 
+//count console.logs and ClientLogger. across the project
 function countLogs(file: string) {
   const content = fs.readFileSync(file);
   content
     .toString()
     .split("\n")
     .forEach((f) => {
-      if (f.includes("console.log") || f.includes("ClientLogger.")) {
+      if (f.includes("console.") || f.includes("ClientLogger.")) {
         logs++;
+        if (f.trim().startsWith("//")) {
+          commentedLogs++;
+        }
       }
     });
+}
+
+//comment all logs that are not commented
+function commentLogs(file: string) {
+  const content = fs.readFileSync(file, "utf-8");
+  let contentArr = content.toString().split("\n");
+  let parenBalance = true;
+  let updatedContentArr: string[] = [];
+  contentArr.forEach((l) => {
+    if (l.includes("import") && l.includes("ClientLogger")) {
+      l = "//" + l;
+    }
+    if (
+      l.includes("console.") ||
+      l.includes("ClientLogger.") ||
+      !parenBalance
+    ) {
+      if (!l.trim().startsWith("//") || !parenBalance) {
+        l = "//" + l;
+      }
+      if (l.trim().endsWith(")") || l.trim().endsWith(");")) {
+        parenBalance = true;
+      } else {
+        parenBalance = false;
+      }
+    }
+    updatedContentArr.push(l);
+  });
+  let updatedContent: string = updatedContentArr.join("\n");
+  if (content !== updatedContent) {
+    fs.writeFileSync(file, updatedContent, "utf-8");
+    //console.log(`✅ Commented: ${file}`);
+  }
 }
 
 // Remove `console.log(...)` and `ClientLogger.` lines
@@ -57,11 +95,18 @@ const currentDirectory = process.cwd();
 const tsfiles = getFiles(currentDirectory);
 const args = process.argv.slice(2);
 if (args.length === 0) {
-  ServerLogger.error("Provide an argument like --count or --remove");
+  ServerLogger.error(
+    "Provide an argument like --count or --remove or --comment"
+  );
   process.exit();
 } else if (args[0] === "--count") {
   tsfiles.forEach((ts) => countLogs(ts));
-  ServerLogger.success(`Found ${logs} logs`);
+  ServerLogger.success(`Found ${logs} logs in total`);
+  ServerLogger.info(`${commentedLogs} commented`);
+  ServerLogger.info(`${logs - commentedLogs} uncommented`);
 } else if (args[0] === "--remove") {
   tsfiles.forEach((ts) => cleanFile(ts));
+} else if (args[0] === "--comment") {
+  tsfiles.forEach((ts) => commentLogs(ts));
+  ServerLogger.success("✅ All logs commented");
 }
